c925863871ff76db30dc3aefdc9b77047d2e02e1
Erich O. Pintar <59430256+saghonfly@users.noreply.github.com>
2020-05-15 11:07:35 +0200

    Update

--- code/OTA/upgrade.php
+++ code/OTA/upgrade.php
@@ -1,7 +1,19 @@
 <?PHP
+include("logging.php");
+
+loging2file('SHRDZM upgrade called');
 
 header('Content-type: text/plain; charset=utf8', true);
 
+function check_header($name, $value = false) {
+	if(!isset($_SERVER[$name])) {
+		return false;
+	}
+	if($value && $_SERVER[$name] != $value) {
+		return false;
+	}
+	return true;
+}
 
 function sendFile($path) {
     header($_SERVER["SERVER_PROTOCOL"].' 200 OK', true, 200);
@@ -12,10 +24,49 @@ function sendFile($path) {
     readfile($path);
 }
 
+if(!check_header('HTTP_USER_AGENT', 'ESP8266-http-Update')) {
+	header($_SERVER["SERVER_PROTOCOL"].' 403 Forbidden', true, 403);
+	echo "only for SHRDZM updater!\n";
+	exit();
+}
+
+if(
+	!check_header('HTTP_X_ESP8266_STA_MAC') ||
+	!check_header('HTTP_X_ESP8266_AP_MAC') ||
+	!check_header('HTTP_X_ESP8266_FREE_SPACE') ||
+	!check_header('HTTP_X_ESP8266_SKETCH_SIZE') ||
+	!check_header('HTTP_X_ESP8266_CHIP_SIZE') ||
+	!check_header('HTTP_X_ESP8266_SDK_VERSION') ||
+	!check_header('HTTP_X_ESP8266_VERSION')
+) {
+	header($_SERVER["SERVER_PROTOCOL"].' 403 Forbidden', true, 403);
+	echo "only for SHRDZM updater! (header)\n";
+	exit();
+}
+
 $localBinary = "./bin/SHRDZMSensor.bin";
 
-sendFile($localBinary);
+loging2file($_SERVER['HTTP_USER_AGENT']);
+loging2file($_SERVER['HTTP_X_ESP8266_STA_MAC']);
+loging2file($_SERVER['HTTP_X_ESP8266_AP_MAC']);
+loging2file($_SERVER['HTTP_X_ESP8266_FREE_SPACE']);
+loging2file($_SERVER['HTTP_X_ESP8266_SKETCH_SIZE']);
+loging2file($_SERVER['HTTP_X_ESP8266_CHIP_SIZE']);
+loging2file($_SERVER['HTTP_X_ESP8266_SDK_VERSION']);
+loging2file($_SERVER['HTTP_X_ESP8266_VERSION']);
 
-header($_SERVER["SERVER_PROTOCOL"].' 500 no version for ESP MAC', true, 500);
+loging2file(md5_file($localBinary));
+
+if($_SERVER['HTTP_X_ESP8266_VERSION'] != md5_file($localBinary))
+{
+	sendFile($localBinary);
+	exit();
+}
+else
+{
+	header($_SERVER["SERVER_PROTOCOL"].' 304 Not Modified', true, 304);
+}
+
+//header($_SERVER["SERVER_PROTOCOL"].' 500 no version for ESP MAC', true, 500);
 
 ?>
--- code/SHRDZMSensor/SHRDZMSensor.ino
+++ code/SHRDZMSensor/SHRDZMSensor.ino
@@ -588,12 +588,30 @@ void loop()
       firmwareUpdate = false;
       
       ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);         
-      
+
+      String versionStr = ESP.getSketchMD5();
       Serial.println("WLAN connected!");
+      Serial.println(versionStr);
 
       WiFiClient client;  
-      // 3D:FE:FB:13:B3:90:DA:FC:FC:44:8C:D7:4E:64:A0:E5:06:A1:6F:2E
-      t_httpUpdate_return ret = ESPhttpUpdate.update(host, 80, url, "1.0.0");    
+      t_httpUpdate_return ret = ESPhttpUpdate.update(host, 80, url, versionStr);    
+      
+      switch (ret) 
+      {
+        case HTTP_UPDATE_FAILED:
+          Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+          break;
+  
+        case HTTP_UPDATE_NO_UPDATES:
+          Serial.println("HTTP_UPDATE_NO_UPDATES");
+          break;
+  
+        case HTTP_UPDATE_OK:
+          Serial.println("HTTP_UPDATE_OK");
+          break;
+      }
+
+      pairingOngoing = false;
     }
   }
 
