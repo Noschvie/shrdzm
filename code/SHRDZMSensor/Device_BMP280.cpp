d58ff9250042eecbb8eee45a8977794c0533cbfc
Erich O. Pintar <59430256+saghonfly@users.noreply.github.com>
2020-05-31 11:43:59 +0200

    Update

--- code/SHRDZMSensor/Device_BMP280.cpp
+++ code/SHRDZMSensor/Device_BMP280.cpp
@@ -29,13 +29,13 @@ bool Device_BMP280::setDeviceParameter(JsonObject obj)
   }
   else
   {
-    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                    Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                    Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                    Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                    Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+//    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+//                    Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+//                    Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+//                    Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+//                    Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
   
-    bmp_temp->printSensorDetails();    
+//    bmp_temp->printSensorDetails();    
   }
 }
 
