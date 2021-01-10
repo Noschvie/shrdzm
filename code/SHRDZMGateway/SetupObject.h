76bd0d011e654b6f3c06dbd1339b28ef23c5d52a
Erich O. Pintar <59430256+saghonfly@users.noreply.github.com>
2021-01-10 14:35:31 +0100

    Back to 0.3.3

---
+++ code/SHRDZMGateway/SetupObject.h
@@ -0,0 +1,119 @@
+class SetupObject
+{
+  public:
+  SetupObject()
+  {
+    m_simpleEspConnection = NULL;
+  }
+  
+  class SetupItem
+  {
+    public:
+    String m_deviceName;
+    String m_parameterName;
+    String m_parameterValue;
+
+    public:
+    SetupItem(String deviceName, String parameterName, String parameterValue)
+    {
+      m_deviceName = deviceName;
+      m_parameterName = parameterName;
+      m_parameterValue = parameterValue;
+    };
+    SetupItem(String deviceName, String command)
+    {
+      m_deviceName = deviceName;
+      m_parameterName = command;
+      m_parameterValue = "";
+    };
+  };
+
+  protected:
+  SimpleEspNowConnection *m_simpleEspConnection;
+
+  public:
+  SetupItem *items[20];
+
+  void init(SimpleEspNowConnection *simpleEspConnection)
+  {
+    m_simpleEspConnection = simpleEspConnection;
+  }
+  
+  void AddItem( String deviceName, String parameterName, String parameterValue )
+  {
+    for(int i = 0; i<20; i++)
+    {
+      if(items[i] == NULL)
+      {
+        items[i] = new SetupItem(deviceName, parameterName, parameterValue);
+        break;
+      }
+    }
+
+/*    if(m_simpleEspConnection != NULL)
+    {
+      m_simpleEspConnection->sendMessage("$PING$", deviceName);  
+    } */
+  };
+
+  void AddItem( String deviceName, String command )
+  {
+    for(int i = 0; i<20; i++)
+    {
+      if(items[i] == NULL)
+      {
+        items[i] = new SetupItem(deviceName, command);
+        break;
+      }
+    }
+
+/*    if(m_simpleEspConnection != NULL)
+    {
+      m_simpleEspConnection->sendMessage("$PING$", deviceName);  
+    }    */
+  };
+  
+  void AddInitItem( String deviceName )
+  {
+    for(int i = 0; i<20; i++)
+    {
+      if(items[i] == NULL)
+      {
+        items[i] = new SetupItem(deviceName, "init", "");
+        break;
+      }
+    }
+  };
+  
+  SetupItem* GetItem( String deviceName )
+  {
+    SetupItem *it = NULL;
+    
+    for(int i = 0;i<20; i++)
+    {
+      if(items[i] != NULL)
+      {
+        if(items[i]->m_deviceName == deviceName)
+        {
+          it = items[i];
+        }
+        break;
+      }
+    }
+
+    return it;
+  };  
+
+  void RemoveItem(SetupItem *it)
+  {
+    for(int i = 0;i<20; i++)
+    {
+      if(items[i] != NULL && items[i] == it)
+      {
+        delete items[i];
+        items[i] = NULL;
+        break;
+      }
+    }
+  };
+};
--- code/SHRDZMGateway/config/config.h
+++ code/SHRDZMGateway/config/config.h
@@ -1,35 +1,16 @@
-#define VERSION		"0.4.0"
+#define VERSION		"0.3.3"
 #define NAME		"SHRDZMGateway"
 
 
+#define DEBUG
 #define LEDPIN 2
-//#define LITTLEFS
+#define WIFI_CHANNEL 4
+#define SEND_TIMEOUT 1000  // 245 millis seconds timeout 
 
 #define ACCESSPOINT_PIN	13
+
 #define SERIALBAUD 	9600
 
-#ifdef DEBUG_SHRDZM
-#define D(...)   Serial.print (__VA_ARGS__); Serial.print(F(" "))
-#define DLN(...)   Serial.print (__VA_ARGS__); Serial.println()
-#define DV(...)   Serial.print(F(#__VA_ARGS__" = ")); Serial.print (__VA_ARGS__);Serial.print(F(" ")); Serial.println()
-#else
-#define D(...)    
-#define DLN(...)  
-#define DV(...)  
-#endif
-
-#include "SimpleEspNowConnection.h"
-
-#include <ArduinoJson.h>
-#include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266httpUpdate.h>
-#include <ESP8266WebServer.h>
-#include "StringSplitter.h"
-
-
-#include "..\Configuration.h"
-
-#ifdef LITTLEFS
-#include <LittleFS.h>
-#endif
+#include <ESP8266WiFiMulti.h>
