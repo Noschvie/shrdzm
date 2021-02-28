1e0a0131233f87047c81f22779e5982538b7cfc1
Erich O. Pintar <59430256+saghonfly@users.noreply.github.com>
2021-02-28 18:34:03 +0100

    Update

--- code/SHRDZMDevice/Device_DOORSENSOR.cpp
+++ code/SHRDZMDevice/Device_DOORSENSOR.cpp
@@ -46,7 +46,7 @@ SensorData* Device_DOORSENSOR::readInitialSetupParameter()
   SensorData *al = new SensorData(2);
 
   al->di[0].nameI = "interval";
-  al->di[0].valueI = "120";
+  al->di[0].valueI = "-1";
   al->di[1].nameI = "preparetime";
   al->di[1].valueI = "0";
 
